ec84a84876ba3bc880cbc367a1f55c3748831fc5
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-05-28 09:52:29 -0500

    butter cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,89 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const posts = data.posts.edges.map(({ node }) => {
+    return node
+  })
+
+  const post = posts[0]
+
+  return (
+    <Layout>
+      <SEO
+        title={post.seo_title}
+        keywords={[`gatsby`, `application`, `react`]}
+      />
+             <div
+          style={{
+            display: `flex`,
+            flexDirection: `column`,
+            alignItems: `center`,
+            justifyContent: `center`,
+          }}
+        >
+          <img
+            key={post.featured_image}
+            style={{ width: `200px`, borderRadius: `10px` }}
+            src={post.featured_image}
+          />
+        </div>
+      <div
+        style={{
+          height: `50%`,
+          display: `flex`,
+          padding: `1rem`,
+          alignItems: `center`,
+          justifyContent: `center`,
+          flexDirection: `column`,
+          background: `linear-gradient(-45deg, rgb(29, 64, 86) 0%, rgb(60, 24, 78) 100%)`,
+        }}
+      >
+        <h1
+          style={{
+            textAlign: `center`,
+            color: `white`,
+            fontSize: `2.5rem`,
+            fontWeight: `100`,
+            maxWidth: `960px`,
+          }}
+        >
+          {post.title}
+        </h1>
+
+ 
+
+        <p
+          style={{
+            textAlign: `center`,
+            color: `white`,
+            fontSize: `1rem`,
+            fontWeight: `100`,
+            maxWidth: `960px`,
+          }}
+          dangerouslySetInnerHTML={{ __html: post.body }}
+        />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    posts: allButterPost {
+      edges {
+        node {
+          id
+          title
+          seo_title
+          featured_image
+          body
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
